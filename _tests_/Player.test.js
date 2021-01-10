8de158c749d0efee49ca68f3f904d86b4f3eecd9
Elysiayn <wendy.lemus.wl@gmail.com>
2021-01-10 04:01:40 -0500

    created prototype methods to getStats and getinventory

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -17,3 +17,24 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)]));
 });
+
+//test for checking player stats as object
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+//test for player inventory to be returned as array
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
