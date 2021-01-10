76bbcf034520ff52a43f735793ce92dabbc4955c
Elysiayn <wendy.lemus.wl@gmail.com>
2021-01-10 05:19:21 -0500

    completed player constructor

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -3,7 +3,7 @@ const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
+//console.log(new Potion()); only prints as Potion {} <anon>
 
 
 //test that checks for the existence of health/strength/agility
@@ -69,4 +69,34 @@ test("subtracts from player's health", () => {
     player.reduceHealth(99999);
 
     expect(player.health).toBe(0);
+});
+
+//test to get a player's attack value
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//test checks that a potion is added correctly to Player inventory
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+//test to ensure potion is removed from inv and stat is updated on use
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
 });
